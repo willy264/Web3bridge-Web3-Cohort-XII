c4b926db124477cf3c1d5f3e606d698b116f0634
Agnilonda Pakou <pagnilonda@gmail.com>
2025-01-30 18:59:16 +0100

    Ready for deply on meter

--- tasks/week-3/ballot-assessment/hardhat.config.ts
+++ tasks/week-3/ballot-assessment/hardhat.config.ts
@@ -1,7 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";require("dotenv").config();
 
-const {ALCHEMY_SEPOLIA_API_KEY_URL, ALCHEMY_AMOY_API_KEY_URL, LISK_API_KEY_URL, ACCOUNT_PRIVATE_KEY} = process.env;
+const {LISK_API_KEY_URL, ALCHEMY_AMOY_API_KEY_URL, ALCHEMY_SEPOLIA_API_KEY_URL, METER_API_KEY_URL, ACCOUNT_PRIVATE_KEY} = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
@@ -9,10 +9,22 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {
     },
+    sepolia: {
+      url: ALCHEMY_SEPOLIA_API_KEY_URL,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    },
     liskSepolia: {
       url: LISK_API_KEY_URL,
       accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
-    }
+    },
+    amoy: {
+      url: ALCHEMY_AMOY_API_KEY_URL,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    },
+    meter: {
+      url: METER_API_KEY_URL,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    },
   }
 };
 
