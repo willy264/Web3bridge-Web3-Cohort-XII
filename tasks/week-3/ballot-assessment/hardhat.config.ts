80ef1607998488ed2b8ce4a900eef14eb2bb5fd0
Agnilonda Pakou <pagnilonda@gmail.com>
2025-01-30 18:41:32 +0100

    Deploy on lisk sepolia

--- tasks/week-3/ballot-assessment/hardhat.config.ts
+++ tasks/week-3/ballot-assessment/hardhat.config.ts
@@ -1,7 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";require("dotenv").config();
 
-const {ALCHEMY_AMOY_API_KEY_URL, ACCOUNT_PRIVATE_KEY} = process.env;
+const {ALCHEMY_SEPOLIA_API_KEY_URL, ALCHEMY_AMOY_API_KEY_URL, LISK_API_KEY_URL, ACCOUNT_PRIVATE_KEY} = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
@@ -9,8 +9,8 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {
     },
-    polygoneAmoy: {
-      url: ALCHEMY_AMOY_API_KEY_URL,
+    liskSepolia: {
+      url: LISK_API_KEY_URL,
       accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
     }
   }
