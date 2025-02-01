cf874b1561ff2af455c5249bc27ff93c4d8fd035
Geoffrey Logovi <52314615+geoffreylgv@users.noreply.github.com>
2025-02-01 01:29:59 +0000

    update and verification done

--- tasks/week-3/Day-5/GeoffreyLogovi/hardhat.config.ts
+++ tasks/week-3/Day-5/GeoffreyLogovi/hardhat.config.ts
@@ -3,7 +3,7 @@ import "@nomicfoundation/hardhat-toolbox";
 
 require("dotenv").config();
 
-const {ALCHEMY_SEPOLIA_API_KEY_URL, ACCOUNT_PRIVATE_KEY} = process.env;
+const { ALCHEMY_SEPOLIA_API_KEY_URL, ACCOUNT_PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
@@ -15,8 +15,11 @@ const config: HardhatUserConfig = {
       url: ALCHEMY_SEPOLIA_API_KEY_URL,
       accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
     },
-   
-  }
+  },
+  
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  },
 };
 
 export default config;
