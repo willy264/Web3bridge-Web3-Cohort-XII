49c3d29abfe49db8203ca264f53fa88bad8adf07
qadir-adesoye <abdqadirola@gmail.com>
2025-02-04 16:16:31 +0100

    qadir adesoye card checker

--- submissions/week-4/Day-1/card-checker/contracts/CardValidator.sol
+++ submissions/week-4/Day-1/card-checker/contracts/CardValidator.sol
@@ -1,18 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract CardValidator {
-    
+contract CardValidator {    
     function isValidCard(uint256 cardNumber) public pure returns (bool) {
         require(cardNumber >= 1e15 && cardNumber < 1e16, "Invalid card length");
 
         uint256 sum = 0;
-        bool doubleDigit = false;
-        uint256 tempNumber = cardNumber;
+        bool doubleDigit = false;        uint256 tempNumber = cardNumber;
 
         for (uint8 i = 0; i < 16; i++) {
             uint256 digit = tempNumber % 10;
             tempNumber /= 10;
+
 
             if (doubleDigit) {
                 digit *= 2;
