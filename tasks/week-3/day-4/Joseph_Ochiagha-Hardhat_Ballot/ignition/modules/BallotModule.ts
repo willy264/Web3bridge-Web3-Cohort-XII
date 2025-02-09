2872d219ad20c448bb22d091b1b526ae2a1915ee
U~Iriamuzu <daviswill264@gmail.com>
2025-02-09 03:16:57 +0100

    sync to code

--- tasks/week-3/day-4/Joseph_Ochiagha-Hardhat_Ballot/ignition/modules/BallotModule.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const BallotModule = buildModule("BallotModule", (m) => {
-  const ballot = m.contract("Ballot");
-
-  return { ballot };
-});
-
-export default BallotModule;
