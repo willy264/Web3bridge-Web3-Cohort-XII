c1c2759b37b0accc30e500ab916f89fb23aaeed6
Samuel1505 <oluwakoredeakinola@gmail.com>
2025-02-11 00:56:34 +0100

    week4 PiggyBank for eth and Nft

--- submissions/week-3/day-5/Joseph_Ochiagha-Decentralized_Voting/ignition/modules/Voting.ts
+++
@@ -1,7 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("VotingModule", (m) => {
-    const candidateNames = ["Alice", "Bob", "Charlie"];
-    const votingContract = m.contract("DecentralizedVoting", [candidateNames]);
-    return { votingContract };
-});
--- tasks/week-4/Day-1-task.md
+++
@@ -1,3 +0,0 @@
-Assignment week 4, day 1
-
-Task: solve the puzzle on Day1.jpg image
